036e115b9719b3c48218487c655f3be17a31a3dd
RinnAnd <107440163+RinnAnd@users.noreply.github.com>
2023-04-11 15:24:11 -0500

    env

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const { cors } = require("../src/middleware/cors.js");
 const socketIo = require('./socket.js')
+require('dotenv').config()
 
 const app = express();
 
@@ -15,7 +16,7 @@ const server = http.createServer(app);
 
 socketIo(server);
 
-const port = 1234;
+const port = process.env.PORT || 1234;
 
 server.listen(port, () => {
   console.log(`Server is now up and running on port ${port}`);
